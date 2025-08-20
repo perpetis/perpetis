1446616a3fcb77dc17b074848f756a8dd2901231
Qiang Li <liqiang@gmail.com>
2025-08-19 17:12:50 -0700

    Update init.sh

--- data/postgres/initdb.d/init.sh
+++ data/postgres/initdb.d/init.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 set -eu
 
-##
-function create_db() {
-	local database=$1
-	local user=$2
+# ##
+# function create_db() {
+# 	local database=$1
+# 	local user=$2
 
-	echo "  Creating user and database '$database'"
-	psql --username $user --no-password -v ON_ERROR_STOP=1 <<-EOSQL
-	    CREATE USER $database;
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
-EOSQL
-    psql --username $user --no-password -d "${database}" -c "CREATE EXTENSION IF NOT EXISTS pgcrypto"
-    psql --username $user --no-password -d "${database}" -c "CREATE EXTENSION IF NOT EXISTS vector"
-}
+# 	echo "  Creating user and database '$database'"
+# 	psql --username $user --no-password -v ON_ERROR_STOP=1 <<-EOSQL
+# 	    CREATE USER $database;
+# 	    CREATE DATABASE $database;
+# 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+#         GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
+# EOSQL
+#     psql --username $user --no-password -d "${database}" -c "CREATE EXTENSION IF NOT EXISTS pgcrypto"
+#     psql --username $user --no-password -d "${database}" -c "CREATE EXTENSION IF NOT EXISTS vector"
+# }
 
-#
-create_db documents admin
-create_db downloads admin
-create_db movies admin
-create_db music admin
-create_db pictures admin
+# #
+# create_db documents admin
+# create_db downloads admin
+# create_db movies admin
+# create_db music admin
+# create_db pictures admin
 
-#
-create_db litellm admin
+# #
+# create_db litellm admin
 
 ##
